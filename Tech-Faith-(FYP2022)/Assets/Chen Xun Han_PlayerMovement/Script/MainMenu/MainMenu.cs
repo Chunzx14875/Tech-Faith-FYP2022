db79b63e8a3d0951012f82b341ab86ed7803ee19
XunHanChen <xunhan65@gmail.com>
2022-12-01 16:53:16 +0800

    Add main menu scene (sample)

---
+++ Tech-Faith-(FYP2022)/Assets/Chen Xun Han_PlayerMovement/Script/MainMenu/MainMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("MAIN")]
+    [SerializeField] GameObject MainMenuButton;
+    private GameObject[] pageButtons;
+
+    [Header("START")]
+    [Space(25)]
+    [SerializeField] GameObject J;
+
+    [Header("SETTING")]
+    [Space(25)]
+    [SerializeField] GameObject S;
+
+    [Header("QUIT")]
+    [Space(25)]
+    [SerializeField] GameObject DS;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void startButton()
+    {
+
+    }
+}
