51c0845136e7a5f8da3c163ff1a76c658ca7392a
XunHanChen <xunhan65@gmail.com>
2022-12-02 13:40:53 +0800

    Main Menu script (no Animation)

--- Tech-Faith-(FYP2022)/Assets/Chen Xun Han_PlayerMovement/Script/MainMenu/MainMenu.cs
+++ Tech-Faith-(FYP2022)/Assets/Chen Xun Han_PlayerMovement/Script/MainMenu/MainMenu.cs
@@ -2,24 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [Header("MAIN")]
-    [SerializeField] GameObject MainMenuButton;
-    private GameObject[] pageButtons;
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject startMenu;
+    [SerializeField] GameObject settingMenu;
 
     [Header("START")]
     [Space(25)]
-    [SerializeField] GameObject J;
+    [SerializeField] GameObject comfirmButton;
 
     [Header("SETTING")]
     [Space(25)]
-    [SerializeField] GameObject S;
+    [SerializeField] GameObject settingenu;
 
-    [Header("QUIT")]
-    [Space(25)]
-    [SerializeField] GameObject DS;
 
     void Start()
     {
@@ -33,6 +32,55 @@ public class MainMenu : MonoBehaviour
 
     public void startButton()
     {
-
+        startMenu.SetActive(true);
+        mainMenu.SetActive(false);
     }
+
+    public void settingButton()
+    {
+        settingMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void quitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+    public void backButton()
+    {
+        mainMenu.SetActive(true);
+        startMenu.SetActive(false);
+        settingMenu.SetActive(false);
+    }
+
+
+    #region SelectLevel
+    public void tutorialLevel()
+    {
+        SceneManager.LoadScene("Sample Layout");
+    }
+
+    public void level1()
+    {
+        SceneManager.LoadScene("Sample Layout");
+    }
+
+    public void level2()
+    {
+        SceneManager.LoadScene("Sample Layout");
+    }
+
+    public void level3()
+    {
+        SceneManager.LoadScene("Sample Layout");
+    }
+    #endregion
+
+    #region Setting
+    //public void sound()
+    //{
+    //}
+    #endregion
 }
