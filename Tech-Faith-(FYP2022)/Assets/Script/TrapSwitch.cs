13fca2e17dd4fc9bac416f8db6b664375c1aeb38
XunHanChen <xunhan65@gmail.com>
2023-03-25 17:05:04 +0800

    Add control panel function, 3D text, platform(trap), electric device function and make longer ground at the end

--- Tech-Faith-(FYP2022)/Assets/Script/TrapSwitch.cs
+++ Tech-Faith-(FYP2022)/Assets/Script/TrapSwitch.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class TrapSwitch : MonoBehaviour
 {
+    [Header("Trap")]
     [SerializeField] GameObject Trap;
 
     Rigidbody rb;
     [SerializeField] private float KnockBackForce = -50;
 
-    private void Start()
+    [Space(25)]
+    [Header("MovingPlate")]
+    private Animator animator;
+    [SerializeField] private GameObject activeElecDevice;
+    private string currentState;
+
+    const string ELEC_DEVICE = "Smallplate";
+
+    void Start()
     {
+        animator = activeElecDevice.GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "ElectricField")
+        //if (other.tag == "ElectricField")
+        //{
+        //    Vector3 direction = (other.transform.position - transform.position).normalized;
+        //    direction.y = -KnockBackForce;
+
+        //    rb.AddForce(direction * KnockBackForce, ForceMode.Impulse);
+
+        //    Destroy(Trap);
+
+        //    Debug.Log("destroy");
+
+        //}
+
+        if (other.tag == "Bolt")
         {
             Vector3 direction = (other.transform.position - transform.position).normalized;
             direction.y = -KnockBackForce;
@@ -24,10 +47,21 @@ public class TrapSwitch : MonoBehaviour
             rb.AddForce(direction * KnockBackForce, ForceMode.Impulse);
 
             Destroy(Trap);
-
             Debug.Log("destroy");
 
+            ChangeAnimationState(ELEC_DEVICE);
         }
     }
 
+    void ChangeAnimationState(string newState)
+    {
+        //Stop the same animation from interrupting itself
+        if (currentState == newState) return;
+
+        //Play the animation
+        animator.Play(newState);
+
+        //Reassign the current state
+        currentState = newState;
+    }
 }
