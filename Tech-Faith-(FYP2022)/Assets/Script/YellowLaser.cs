234e6b83dfb5cfb5071745d7b9f9b480ce43eba3
Chunzx14875 <chinzixian14875@gmail.com>
2023-03-26 12:26:15 +0800

    Done Laser Puzzle in SampleScene

---
+++ Tech-Faith-(FYP2022)/Assets/Script/YellowLaser.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowLaser : MonoBehaviour
+{
+    [SerializeField] private int reflections;
+    [SerializeField] private float MaxLength;
+
+    private LineRenderer lr;
+    private Ray ray;
+    private RaycastHit hit;
+    private Vector3 direction;
+
+    [SerializeField] GameObject Box;
+    [SerializeField] Transform SpawnBox;
+    bool spawnBox = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = new Ray(transform.position, transform.forward);
+
+        lr.positionCount = 1;
+        lr.SetPosition(0, transform.position);
+        float remaingLength = MaxLength;
+
+        for (int i = 0; i < reflections; i++)
+        {
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, remaingLength))
+            {
+                lr.positionCount += 1;
+                lr.SetPosition(lr.positionCount - 1, hit.point);
+                remaingLength -= Vector3.Distance(ray.origin, hit.point);
+                ray = new Ray(hit.point, Vector3.Reflect(ray.direction, hit.normal));
+                if (hit.collider.GetComponent<YellowMirror>() == null)
+                {
+                    if (hit.collider.gameObject.CompareTag("Player"))
+                    {
+                        Debug.Log("Hit Player");
+                        break;
+                    }
+                    if (hit.collider.gameObject.name == "YellowPicture")
+                    {
+                        if (!spawnBox)
+                        {
+                            Instantiate(Box, SpawnBox.position, SpawnBox.rotation);
+                            spawnBox = true;
+                            Debug.Log("Drop Yellow Box!");
+                        }
+
+                        break;
+                    }
+
+                    break;
+                }
+            }
+            else
+            {
+                lr.positionCount += 1;
+                lr.SetPosition(lr.positionCount - 1, ray.origin + ray.direction * remaingLength);
+            }
+        }
+    }
+}
